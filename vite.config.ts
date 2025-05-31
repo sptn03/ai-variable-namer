456694425f973b0563ea46e33717af1e167007f7
Nam Duong <namsptn03@gmail.com>
2025-05-31 08:05:23 +0700

    up

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/ai-variable-namer/',
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
@@ -15,3 +16,4 @@ export default defineConfig(({ mode }) => {
       }
     };
 });
+
